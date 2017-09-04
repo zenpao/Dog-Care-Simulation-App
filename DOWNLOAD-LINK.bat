58dfb2bbf4008cfb0f867a8057da1daab96a9e37
Jentzen Paolo Javier <31607014+zenpao@users.noreply.github.com>
2017-09-04 18:20:13 +0800

    Update DOWNLOAD-LINK.bat

--- DOWNLOAD-LINK.bat
+++ DOWNLOAD-LINK.bat
@@ -1,3 +1,3 @@
-ECHO "Redirecting you to Google Drive.."
+echo "File size exceeds 25mb. Redirecting you now to Google Drive.."
 
 START "" https://drive.google.com/open?id=0B698DIyh128LUHlwWW53bzJ4R00
